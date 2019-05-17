a6254d786e47036cd406142f20339cc5537341e9
Natalia Julieta <nataliajulieta@gmail.com>
2019-05-17 11:24:09 -0300

    Formulario de Contato

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 // Configure your Subject Prefix and Recipient here
-$subjectPrefix = '[Contact via website]';
-$emailTo       = '<YOUR_EMAIL_HERE>';
+$subjectPrefix = '[Contato Website Barest]';
+$emailTo       = 'contato@barest.com.br';
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -9,19 +9,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email   = stripslashes(trim($_POST['email']));
     $subject = stripslashes(trim($_POST['subject']));
     $message = stripslashes(trim($_POST['message']));
-    if (empty($name)) {
-        $errors['name'] = 'Name is required.';
-    }
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors['email'] = 'Email is invalid.';
-    }
-    if (empty($subject)) {
-        $errors['subject'] = 'Subject is required.';
-    }
-    if (empty($message)) {
-        $errors['message'] = 'Message is required.';
-    }
-    // if there are any errors in our errors array, return a success boolean or false
+
     if (!empty($errors)) {
         $data['success'] = false;
         $data['errors']  = $errors;
@@ -44,10 +32,11 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $headers .= "X-Originating-IP: " . $_SERVER['SERVER_ADDR'] . PHP_EOL;
         mail($emailTo, "=?utf-8?B?" . base64_encode($subject) . "?=", $body, $headers);
         $data['success'] = true;
-        $data['message'] = 'Congratulations. Your message has been sent successfully';
+        $data['message'] = 'Obrigada! Sua mensagem foi enviada. Breve responderemos. ';
     }
     // return all our data to an AJAX call
-    echo json_encode($data);
+    echo '<script>alert ("' .$data['message'] .'");</script>' ;
+    header('location: index.html');
 }
 
 
