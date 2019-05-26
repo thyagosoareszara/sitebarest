795946f23f86ad7730010d3ea3b1d2fae6e624ef
Natalia Julieta <nataliajulieta@gmail.com>
2019-05-26 10:27:09 -0300

    pesquisa

--- cadPesquisa.php
+++ cadPesquisa.php
@@ -1,6 +1,6 @@
+<? include 'conexao.php'; ?>
 <?php
 // RECEBE VARIAVEIS NESCESSARIAS 
- 
 if (isset($_POST['acao']) && ($_POST['acao'] == 'save')){ 
     $nome = $_POST['nome'];
     $email =  $_POST['email'];
@@ -12,20 +12,31 @@ if (isset($_POST['acao']) && ($_POST['acao'] == 'save')){
     $app = $_POST['app'];
     $data_cadastro = getdate();
 
-    $InsertPesquisa = $pdo->prepare("INSERT INTO tbpesquisa (nome, email, sexo, frequenci_bares, internet, vantagens, sugestao, app, data_cadastro ) VALUES('$nome','$email', '$sexo', '$frequencia_bares', '$internet', '$vantagens', '$sugestao', '$app', '$data_cadastro')");
+    $InsertPesquisa = "INSERT INTO tbpesquisa (nome, email, sexo, frequenci_bares, internet, vantagens, sugestao, app, data_cadastro ) VALUES('$nome','$email', '$sexo', '$frequencia_bares', '$internet', '$vantagens', '$sugestao', '$app', '$data_cadastro')";
 
-    $InsertPesquisa->execute();
+    $insert = mysql_query($InsertPesquisa);
+
+    echo($InsertPesquisa); exit;
 
     if (isset($_POST['email'])){ 
-        $InsertNew = $pdo->prepare("INSERT INTO tbnewsletter (nome, email,data_cadastro ) VALUES('$nome','$email', '$data_cadastro')");    
-
-        $InsertNew->execute();
+        $InsertNew = "INSERT INTO tbnewsletter (nome, email,data_cadastro ) VALUES('$nome','$email', '$data_cadastro')";  
+        
+        mysql_query($InsertNew,$conect);
     
     }
+    
+    if(mysql_affected_rows() == 1){ //verifica se foi afetada alguma linha, nesse caso inserida alguma linha
+        $sucesso = 'Obrigada por nos ajudar!!';
+        echo '<script>alert ("' .$sucesso .'");</script>';
+        header('location: /');
+    } else {
+        $error = 'Erro, não possível inserir no banco de dados!';
+        echo '<script>alert ("' .$error .'");</script>';
+        header('location: /');
+    }   
+    
+    mysql_close($conect); //fecha conexão com banco de dados 
 
-    $sucesso = 'Obrigada por nos ajudar!!';
-    echo '<script>alert ("' .$sucesso .'");</script>';
-    header('location: /');
  
 };
 
